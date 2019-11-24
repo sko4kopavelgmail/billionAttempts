517fe6c13232a1add42a6109687561f9ed07e215
sko4kopavelgmail <sko4kopavelgmail@users.noreply.github.com>
2019-11-24 23:12:09 +0300

    initial commit

---
+++ src/main/java/skp/billionAttempts/BillionAttemptsApplication.java
@@ -0,0 +1,13 @@
+package skp.billionAttempts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillionAttemptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillionAttemptsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/skp/billionAttempts/BillionAttemptsApplicationTests.java
@@ -0,0 +1,13 @@
+package skp.billionAttempts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillionAttemptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
